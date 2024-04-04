5c603303021cccdc77d80fc2c5b85bac7b761fec
avenbreaks <dev-cosmware@gitshock.com>
2024-04-04 09:49:22 +0000

    adding genesis hash
    	modified:   core/genesis.go
    	modified:   params/bootnodes.go
    	modified:   params/config.go

--- params/config.go
+++ params/config.go
@@ -28,7 +28,7 @@ var (
 	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
 	HoleskyGenesisHash = common.HexToHash("0xff9006519a8ce843ac9c28549d24211420b546e12ce2d170c77a8cca7964f23d")
 	// TODO : Adding genesis Hash bootstrapped
-	TrollupGenesisHash = common.HexToHash("")
+	TrollupGenesisHash = common.HexToHash("0xfc55fe17119a6152d1435b9ed818fe1415a65b552f5428ee5fc3d81cc63f70aa")
 	SepoliaGenesisHash = common.HexToHash("0x25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9")
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
 )
